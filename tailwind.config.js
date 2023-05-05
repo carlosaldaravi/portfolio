244f2e91759ea636d638f6de2bc4dc43592f7119
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-05-05 04:48:12 +0200

    update tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,15 @@ module.exports = {
         xxs: "1px",
         xs: "2px",
       },
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      },
+      animation: {
+        appear: 'appear 1s',
+      }
     },
   },
   plugins: [require("@tailwindcss/forms")],
