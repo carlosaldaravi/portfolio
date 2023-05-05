f0b5084ed5136b06ef5233f09c2a467258d01ea3
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-05-05 03:35:34 +0200

    add conf to remember locale

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "@/styles/info-card.scss";
 
 import { IntlProvider } from "react-intl";
 import { useRouter } from "next/router";
+import { setCookie } from 'cookies-next';
 
 import en from "../lang/en.json";
 import es from "../lang/es.json";
@@ -14,6 +15,7 @@ const messages = {
 
 export default function App({ Component, pageProps }) {
   const { locale } = useRouter();
+  setCookie('NEXT_LOCALE', locale);
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
       <Component {...pageProps} />
