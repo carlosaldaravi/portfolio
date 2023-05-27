3ca0787d002a71da827d4a865a2f83fae9a10e21
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-05-27 13:25:19 +0100

    fix styles expanded jump card

--- src/components/kitesurf/jump-card/texts-jump-card.js
+++ src/components/kitesurf/jump-card/texts-jump-card.js
@@ -6,11 +6,11 @@ const TextsJumpCard = ({ texts, index }) => {
       {texts.map((object) => {
         const key = Object.keys(object)[0];
         return (
-          <div className="flex w-full" key={`${object[key]} - ${index}`}>
-            <span className="text-gray-400 tracking-xs uppercase self-center text-end mr-4 text-xs sm:text-xl sm:tracking-sm sm:mr-12 sm:w-96">
+          <div className="flex w-full justify-between" key={`${object[key]} - ${index}`}>
+            <span className="w-[40%] text-gray-400 tracking-xs uppercase self-center text-end mr-4 text-xs sm:text-xl sm:tracking-sm sm:mr-12 sm:w-96">
               <FormattedMessage id={key} />
             </span>
-            <span className="tracking-xxs sm:text-2xl text-gray-200">
+            <span className="tracking-xxs sm:text-2xl text-gray-200 flex-grow-0" style={{ whiteSpace: "nowrap", overflow: "hidden", width: "70%" }}>
               {object[key]}{" "}
               {key === "hangtime" && <FormattedMessage id="seconds" />}
               {key === "wind" && <FormattedMessage id="knots" />}
