0f86f6b06d2178da31f1f89cbcb6ef563f0d532b
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-05-28 22:45:35 +0100

    add animation to youtube icon

--- src/components/kitesurf/jump-card/youtube-icon.js
+++ src/components/kitesurf/jump-card/youtube-icon.js
@@ -1,9 +1,14 @@
+import { useTools } from "../../../hooks/useTools";
 import SVG from "../../svg";
 
-const YoutubeIcon = ({onPlay}) => {
+const YoutubeIcon = ({ onPlay }) => {
+  const { isMobile } = useTools();
+
   return (
     <div
-      className="animate-appear absolute top-1/2 -translate-y-1/2 left-5 cursor-pointer z-50"
+      className={`absolute left-5 sm:left-14 cursor-pointer z-10 ${
+        !isMobile && "clickHere"
+      }`}
       onClick={onPlay}
     >
       <SVG type="youtube" />
