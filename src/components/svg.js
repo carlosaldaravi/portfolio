2964da388408b27e36e000a31ea326b43706bcc9
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-06-05 16:54:45 +0200

    fix back side kite card function

--- src/components/svg.js
+++ src/components/svg.js
@@ -202,7 +202,7 @@ const SVG = ({ type, hover, actualLanguage }) => {
         viewBox="0 0 24 24"
         strokeWidth="1.5"
         stroke="currentColor"
-        className="w-8 h-8 sm:w-12 sm:h-12"
+        className="w-8 h-8 sm:w-12 sm:h-12 z-50 cursor-pointer"
       >
         <path
           strokeLinecap="round"
