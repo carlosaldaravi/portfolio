f16dfa223bbfe00e0d0e149581e96eb2c31569d9
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-06-06 20:39:41 +0200

    update border style

--- src/components/layout/footer-links.js
+++ src/components/layout/footer-links.js
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { FormattedMessage } from "react-intl";
+import ThemeContext from "@/store/theme-context";
 import FooterLink from "./footer-link";
 
 const FooterLinks = () => {
+  const themeCtx = useContext(ThemeContext);
+  const theme = themeCtx.theme;
   return (
     <div className="">
-      <div className="border-b border-gray-200">
+      <div className={`border-b ${theme === "dark" ? "border-light-secondary" : "border-dark-secondary"}`}>
         <nav className="-mb-px flex" aria-label="Tabs">
           <FooterLink src="/" title={<FormattedMessage id="footer.home" />} />
           <FooterLink
