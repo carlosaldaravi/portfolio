90b13f4b853aec88a60a76f88d17ef64bdeb7ee8
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-06-07 01:48:06 +0200

    fix z-index mobile problem

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <div id="myportal"></div>
+        <div id="popup"></div>
         <NextScript />
       </body>
     </Html>
