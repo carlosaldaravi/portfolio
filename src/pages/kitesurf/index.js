6d46db6af5d42f1a05f3e6a42d539c74cac4939b
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-06-12 19:25:44 +0200

    update video opacity

--- src/pages/kitesurf/index.js
+++ src/pages/kitesurf/index.js
@@ -33,7 +33,7 @@ const KiteSurf = ({ sections, me }) => {
 
   return (
     <Page className="relative kitesurf__page__container">
-      <div className="fixed inset-0 z-0 opacity-10">
+      <div className="fixed inset-0 z-0 opacity-50">
         <video
           className="w-full h-screen object-cover"
           src={"/video.MP4"}
