8d867ed5ea7935b10509e78ce0d947ba8b38ae81
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2023-06-22 20:58:13 +0200

    create api endpoint to get ranking

---
+++ src/pages/api/getRanking.js
@@ -0,0 +1,12 @@
+import { SURFR_URL } from "@/env/constants";
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(SURFR_URL + process.env.SURFR_ACCESS_TOKEN);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error connecting to Surfr. API" });
+  }
+}
